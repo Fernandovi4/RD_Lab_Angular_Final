3ebc25f3ea5dc5cb550d1f5b3e41e7e4daa2af08
Petro Rikabal Aviles <ricabalaviles@gmail.com>
2021-09-15 10:54:33 +0300

    fixed start-api script

--- src/app/app-routing-module/app-routing-module.module.ts
+++ src/app/app-routing-module/app-routing-module.module.ts
@@ -1,26 +1,27 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router'
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router'
 
 
-import { LogInComponent } from '../components/login/log-in-component';
-import { GamesComponent } from '../components/games/games.component';
-import { LibraryComponent } from '../components/library/library.component';
-import { FriendsComponent } from '../components/friends/friends.component';
-import { ProfileComponent } from '../components/profile/profile.component';
+import {LogInComponent} from '../components/login/log-in-component';
+import {GamesComponent} from '../components/games/games.component';
+import {LibraryComponent} from '../components/library/library.component';
+import {FriendsComponent} from '../components/friends/friends.component';
+import {ProfileComponent} from '../components/profile/profile.component';
 import {FriendsSearchComponent} from "../components/frinds-search/frinds-search.component";
 
 const routes: Routes = [
-  { path: 'login', component: LogInComponent},
-  { path: 'games', component: GamesComponent},
-  { path: 'library', component: LibraryComponent},
-  { path: 'friends', component: FriendsComponent},
-  { path: 'friends-search', component: FriendsSearchComponent},
-  { path: 'profile', component: ProfileComponent},
-  { path: '**', redirectTo: '/'}
+  {path: 'login', component: LogInComponent},
+  {path: 'games', component: GamesComponent},
+  {path: 'library', component: LibraryComponent},
+  {path: 'friends', component: FriendsComponent},
+  {path: 'friends-search', component: FriendsSearchComponent},
+  {path: 'profile', component: ProfileComponent},
+  {path: '**', redirectTo: '/'}
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes , { useHash: true })],
+  imports: [RouterModule.forRoot(routes, {useHash: true})],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
